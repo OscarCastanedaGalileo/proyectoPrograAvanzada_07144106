d0fe22583fb105ba9771133249dcff76ac0bb7a4
Oscar Castaneda <07144106@galileo.edu>
2025-03-11 22:52:55 -0600

    Agregado nuevos archivos y carpetas en semana2

--- config/database.js
+++ config/database.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose');
+const cors  = require('cors'); 
+const express = require('express'); 
+const app = express();
+app.use(cors());
+app.use(express.json());
 require('dotenv').config();
+
+const corsOptions = {
+    origin: "http://localhost:3000", // Allow Next.js frontend
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 
 mongoose.connect(
     process.env.MONGO_URI, { 
